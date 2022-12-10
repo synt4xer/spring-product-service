574b51272b9c0cb29300fbceacf346cf2390279e
Ferrel John Fernando <f.syntaxer@gmail.com>
2022-12-10 21:21:01 +0700

    Add product module

---
+++ src/main/java/com/syntaxer/product/persistence/service/common/CrudService.java
@@ -0,0 +1,54 @@
+package com.syntaxer.product.persistence.service.common;
+
+import com.syntaxer.product.persistence.Operations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+public abstract class CrudService<T extends Serializable> implements Operations<T> {
+
+    protected abstract JpaRepository<T, Long> getRepository();
+
+    @Override
+    @Transactional(readOnly = true)
+    public T findById(long id) {
+        return getRepository().findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<T> findAll() {
+        return new ArrayList<>(getRepository().findAll());
+    }
+
+    @Override
+    public Page<T> findPaginated(int page, int size) {
+        return getRepository().findAll(PageRequest.of(page, size));
+    }
+
+    @Override
+    public T create(T entity) {
+        return getRepository().save(entity);
+    }
+
+    @Override
+    public T update(T entity) {
+        return getRepository().save(entity);
+    }
+
+    @Override
+    public void delete(T entity) {
+        getRepository().delete(entity);
+    }
+
+    @Override
+    public void deleteById(long entityId) {
+        getRepository().deleteById(entityId);
+    }
+}
---
+++ src/main/java/com/syntaxer/product/persistence/service/impl/ProductServiceImpl.java
@@ -0,0 +1,32 @@
+package com.syntaxer.product.persistence.service.impl;
+
+import com.syntaxer.product.persistence.model.Product;
+import com.syntaxer.product.persistence.repository.ProductRepository;
+import com.syntaxer.product.persistence.service.ProductService;
+import com.syntaxer.product.persistence.service.common.CrudService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ProductServiceImpl extends CrudService<Product> implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Page<Product> findPaginated(Pageable pageable) {
+        return getRepository().findAll(pageable);
+    }
+
+    @Override
+    protected JpaRepository<Product, Long> getRepository() {
+        return this.productRepository;
+    }
+}
